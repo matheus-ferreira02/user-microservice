c83466360aed53051567c5eb1c54fa5f8bdf14f5
matheus-ferreira02 <m.ferreira.araujo2016@gmail.com>
2023-03-22 17:49:48 -0300

    feat: :sparkles: User domain created

--- src/main/java/com/user/microservice/core/domain/User.java
+++ src/main/java/com/user/microservice/core/domain/User.java
@@ -1,4 +1,20 @@
 package com.user.microservice.core.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Data
 public class User {
+    private final long id;
+
+    private final String firstname;
+
+    private final String lastname;
+
+    private final String email;
+
+    private final LocalDate birthdate;
 }
